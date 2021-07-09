8971cb524db4a6e9e138c162efe458962b4c2599
Agam-coder <72142932+Agam-coder@users.noreply.github.com>
2021-07-09 16:46:39 -0700

    this is commit

---
+++ app/src/main/java/com/example/ul/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.ul;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+   @Override
+protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+
+        //Button4.setOnClickListener(new OnClickListener() {
+        //public void onClick(View view) {
+        // Intent intent  = new  Intent (MainActivity.this,ScrollingActivity.class);
+        //startActivity(intent);
+
+        //}
+        // });
+    }
+}
