9ccadd8fa0b704746f1ce3a781ad030ffdbbb0b1
Agam-coder <72142932+Agam-coder@users.noreply.github.com>
2021-08-07 16:13:13 -0700

    new ui

--- app/src/main/java/com/example/ul/MainActivity.java
+++ app/src/main/java/com/example/ul/MainActivity.java
@@ -14,17 +14,81 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+       @Override
+       protected void onCreate(Bundle savedInstanceState) {
+              super.onCreate(savedInstanceState);
+              setContentView(R.layout.activity_main);
+              ImageView Button = (ImageView) findViewById(R.id.vn);
+              ImageView Button2 = (ImageView) findViewById(R.id.ox);
+              ImageView Button3 = (ImageView) findViewById(R.id.rd);
+              ImageView Button4 = (ImageView) findViewById(R.id.pla);
+              ImageView Button5 = (ImageView) findViewById(R.id.vc);
+               ImageView Button6 = (ImageView) findViewById(R.id.am);
 
-   @Override
-protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
+              //ImageView Button4 = (ImageView) findViewById(R.id.imageView4);
 
-        //Button4.setOnClickListener(new OnClickListener() {
-        //public void onClick(View view) {
-        // Intent intent  = new  Intent (MainActivity.this,ScrollingActivity.class);
-        //startActivity(intent);
+              Button.setOnClickListener(new OnClickListener() {
+                     public void onClick(View view) {
+                            Intent intent = new Intent();
+                            intent.setAction(Intent.ACTION_VIEW);
+                            intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                            intent.setData(Uri.parse("https://www.covid19resourcesindia.com/search/all/beds"));
+                            startActivity(intent);
+                     }
+              });
+              Button2.setOnClickListener(new OnClickListener() {
+                     public void onClick(View view) {
+                            Intent intent = new Intent();
+                            intent.setAction(Intent.ACTION_VIEW);
+                            intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                            intent.setData(Uri.parse("https://www.covid19resourcesindia.com/search/all/oxygen-cylinders"));
+                            startActivity(intent);
+                     }
+              });
+           Button3.setOnClickListener(new OnClickListener() {
+               public void onClick(View view) {
+                   Intent intent = new Intent();
+                   intent.setAction(Intent.ACTION_VIEW);
+                   intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                   intent.setData(Uri.parse("https://www.covid19resourcesindia.com/search/all/remdesivir"));
+                   startActivity(intent);
+               }
+           });
+           Button4.setOnClickListener(new OnClickListener() {
+               public void onClick(View view) {
+                   Intent intent = new Intent();
+                   intent.setAction(Intent.ACTION_VIEW);
+                   intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                   intent.setData(Uri.parse("https://www.covid19resourcesindia.com/search/all/plasma"));
+                   startActivity(intent);
+               }
+           });
+           Button5.setOnClickListener(new OnClickListener() {
+               public void onClick(View view) {
+                   Intent intent = new Intent();
+                   intent.setAction(Intent.ACTION_VIEW);
+                   intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                   intent.setData(Uri.parse("https://www.covid19resourcesindia.com/search/all/remdesivir"));
+                   startActivity(intent);
+               }
+           });
+           Button6.setOnClickListener(new OnClickListener() {
+               public void onClick(View view) {
+                   Intent intent = new Intent();
+                   intent.setAction(Intent.ACTION_VIEW);
+                   intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                   intent.setData(Uri.parse("https://www.covid19resourcesindia.com/search/all/ambulance"));
+                   startActivity(intent);
+               }
+           });
+              //Button4.setOnClickListener(new OnClickListener() {
+              //public void onClick(View view) {
+              // Intent intent  = new  Intent (MainActivity.this,ScrollingActivity.class);
+              //startActivity(intent);
 
-        //}
-        // });
-    }
+              //}
+              // });
+       }
 }
+
+
